953ae02745e746e8d1655447cc0c7a7b228667f2
Yudi Andela <yudhi.andhela@gmail.com>
2018-12-10 21:46:33 +0700

    Ubah method LoginTest

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,22 +2,24 @@
 
 namespace Tests\Browser;
 
+use App\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class LoginTest extends DuskTestCase
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    /** @test */
+    public function UserLogin()
     {
-        $this->browse(function (Browser $browser) {
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
             $browser->visit('/login')
-                    ->type('email', 'login.test@mail.com')
+                    ->assertSee('E-Mail Address')
+                    ->type('email', $user->email)
                     ->type('password', 'secret')
                     ->press('Login')
                     ->assertSee('You are logged in!');
